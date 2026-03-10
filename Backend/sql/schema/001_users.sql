7a63399d07cd0549dc4bc3f4fd1f8cf595860a30
oneelabed <oneelabed123@gmail.com>
2026-03-10 15:02:07 +0200

    modified username field in users to be unique

--- Backend/sql/schema/001_users.sql
+++ Backend/sql/schema/001_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    username TEXT NOT NULL,
+    username TEXT UNIQUE NOT NULL,
     password_hash TEXT
 );
 
