9cf231cf6a2545c9f00380bdd40be39d756474d9
oneelabed <oneelabed123@gmail.com>
2026-03-20 11:49:13 +0200

    made scraping interval smaller

--- Backend/main.go
+++ Backend/main.go
@@ -42,7 +42,7 @@ func main() {
 		DB: database.New(conn),
 	}
 
-	go StartScraping(apiCfg.DB, 23, time.Minute*30)
+	go StartScraping(apiCfg.DB, 23, time.Minute*20)
 	startCleanupWorker(&apiCfg)
 
 	router := chi.NewRouter()
